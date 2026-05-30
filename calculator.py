d5b3497c25a72e1336ed51aafbed442c22278809
gopal955892-dev <gopal955892@gmail.com>
2026-05-30 11:13:02 +0530

    add calculator project

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# calculator
+a=int(input("Enter any numbear :"))
+c=input("Enter a any Arithmetic operator :")
+b=int(input("Enter any numbear :"))
+
+if (c=="+"):
+    print(a+b)
+elif(c=="-"):
+    print(a-b)
+elif(c=="*"):
+    print(a*b)
+elif(c=="/"):
+    print(a/b)
+else:
+    print("Opretor are not matched")
